d184bbe01e79d44622f993855193dc95855c2418
Vineeth.TR <vineethtrv@gmail.com>
2026-06-28 12:52:29 +0530

    Update base path in vite config for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/cssloaders.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
